2be2aaa7314a5c949c9c8c9338dbc710ac8c60f6
vmogilevskiy <oracle.ux@gmail.com>
2014-07-31 20:01:01 +0000

    added CLIENT_IDENTIFIER

--- linux/mon/evnt/seed/bin/drilsql.sh
+++ linux/mon/evnt/seed/bin/drilsql.sh
@@ -5,6 +5,8 @@
 #					adding "+0" on the txt.hash_value join
 #	11-04-2013	VMOGILEV	(1.3) added decode to get the PREV sql
 #	11-19-2013	VMOGILEV	(1.4) added SQL_ID, PREV_SQL_ID and CLIENT_INFO
+#	JUL-31-2014	VMOGILEV	(1.5) added CLIENT_IDENTIFIER
+CLIENT_IDENTIFIER
 
 sesfile=$1
 outfile=$2
@@ -34,6 +36,7 @@ col action format a45 noprint new_value n_action
 col SQL_ID format a13 noprint new_value n_SQL_ID
 col PREV_SQL_ID format a13 noprint new_value n_PREV_SQL_ID
 col CLIENT_INFO format a64 noprint new_value n_CLIENT_INFO
+col CLIENT_IDENTIFIER format a64 noprint new_value n_CLIENT_IDENTIFIER
 
 break on sid on serial on username on process on machine on action skip page
 
@@ -50,6 +53,8 @@ ttitle -
       skip 1 -
        "Client Info ....... : "  n_CLIENT_INFO -
       skip 1 -
+       "Client Id ......... : "  n_CLIENT_IDENTIFIER -
+      skip 1 -
        "Machine ........... : "  n_machine -
       skip 1 -
        "OSuser ............ : "  n_osuser -
@@ -62,7 +67,8 @@ select /*+ ORDERED */
    sid,serial# serial,username,machine,osuser,process,module||' '||action action,sql_text,
    nvl(txt.SQL_ID,'null') SQL_ID,
    nvl(ses.PREV_SQL_ID,'null') PREV_SQL_ID,
-   nvl(ses.CLIENT_INFO,'null') CLIENT_INFO
+   nvl(ses.CLIENT_INFO,'null') CLIENT_INFO,
+   nvl(ses.CLIENT_IDENTIFIER,'null') CLIENT_IDENTIFIER
 from v\$session ses, v\$sqltext txt
 --from v\$session ses, v\$sqltext_with_newlines txt
 where txt.address(+) = decode(ses.sql_address,'00',ses.PREV_SQL_ADDR,ses.sql_address)
