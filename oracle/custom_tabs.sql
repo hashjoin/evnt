2fe8fa4c3dc4c8d99e91d2c53b4334eedad88267
vmogilevskiy <oracle.ux@gmail.com>
2014-08-01 22:47:05 +0000

    created table event_triggers_sum

--- oracle/custom_tabs.sql
+++ oracle/custom_tabs.sql
@@ -1,18 +1,18 @@
--- $header custom_tabs.sql v1.7 2013-Oct-21 VMOGILEVSKIY 
+-- $header custom_tabs.sql v1.7 2013-Oct-21 VMOGILEVSKIY
 
-CREATE GLOBAL TEMPORARY TABLE EVNT_UTIL_PKG_OUT ( 
-  EUPO_ID        NUMBER (38)   NOT NULL, 
-  EUPO_DATE      DATE          NOT NULL, 
-  EUPO_REF_ID    NUMBER (38)   NOT NULL, 
-  EUPO_REF_TYPE  VARCHAR2 (50)  NOT NULL, 
+CREATE GLOBAL TEMPORARY TABLE EVNT_UTIL_PKG_OUT (
+  EUPO_ID        NUMBER (38)   NOT NULL,
+  EUPO_DATE      DATE          NOT NULL,
+  EUPO_REF_ID    NUMBER (38)   NOT NULL,
+  EUPO_REF_TYPE  VARCHAR2 (50)  NOT NULL,
   EUPO_OUT       VARCHAR2 (4000))
 ON COMMIT PRESERVE ROWS;
 
 CREATE UNIQUE INDEX EVNT_UTIL_PKG_OUT_U01 ON
    EVNT_UTIL_PKG_OUT ( EUPO_ID ) ;
 
-CREATE GLOBAL TEMPORARY TABLE GLOB_PEND_ASSIGNMENTS ( 
-  GPA_SEQ   NUMBER (38)   NOT NULL, 
+CREATE GLOBAL TEMPORARY TABLE GLOB_PEND_ASSIGNMENTS (
+  GPA_SEQ   NUMBER (38)   NOT NULL,
   GPA_VAL   NUMBER (38)   NOT NULL)
 ON COMMIT PRESERVE ROWS;
 
@@ -21,3 +21,28 @@ create table purge_et_id_tmp(
 , constraint peit_pk primary key(et_id)
 )
 organization index;
+
+
+create table event_triggers_sum
+(
+    ea_id           NUMBER(15) not NULL
+,   scnt            NUMBER(15) not NULL
+,   et_phase_status VARCHAR2(1 CHAR) not NULL
+);
+
+
+    SELECT ea_id, count(*) scnt
+    FROM   event_triggers
+    WHERE  et_phase_status = 'P'
+    AND    et_status != 'CLEARED'
+    GROUP BY ea_id) pend
+,    (SELECT ea_id, count(*) scnt
+    FROM   event_triggers
+    WHERE  et_phase_status = 'O'
+    AND    et_status != 'CLEARED'
+    GROUP BY ea_id) old
+,    (SELECT ea_id, count(*) scnt
+    FROM   event_triggers
+    WHERE  et_phase_status = 'C'
+    AND    et_status != 'CLEARED'
+    GROUP BY ea_id
